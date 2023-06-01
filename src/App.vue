88dc464a90d84eeee6611b6eb4d034553246f78e
汪~汪~汪~ <w15755583882022@163.com>
2023-06-02 00:10:36 +0800

    项目初始化的配置

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>我是App</h1>
+  </div>
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  export default defineComponent({
+    name:'App'
+  })
+</script>
+<script lang="ts" setup>
+</script>
+
+<style lang="less" scoped>
+</style>
