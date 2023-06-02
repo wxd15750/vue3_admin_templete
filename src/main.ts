103a1fb828423e846e78a3e4686d9fadb060d5c3
汪~汪~汪~ <w15755583882022@163.com>
2023-06-02 16:17:05 +0800

    feat: 配置项目的运行环境及文件夹起别名

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
-import { createApp } from "vue";
+import { createApp } from 'vue'
+// @ts-ignore
+import App from '@/App.vue'
 
-import App from "./App.vue";
+// 引入elements-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 引入国际化
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-createApp(App).mount("#app");
-
+const app = createApp(App)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+app.mount('#app')
