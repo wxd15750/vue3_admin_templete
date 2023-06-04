6b8b82d6c5e446acdfcc5f5d4e6d38ec6089a5e3
汪~汪~汪~ <w15755583882022@163.com>
2023-06-04 10:36:52 +0800

    feat: 对API进行同一管理

---
+++ src/api/user/index.ts
@@ -0,0 +1,4 @@
+// 接口请求函数
+export default {
+
+}
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,7 @@
 // 引入axios
 import axios from 'axios'
 // @ts-ignore
-import { ElMessage } from "element-plus";
+import { ElMessage } from 'element-plus'
 
 // 第一步：利用axios对象的create方法，去创建axios实例（其他的配置:基础路径、超时时间）
 
@@ -13,50 +13,49 @@ const requeset = axios.create({
 })
 
 // // 第二步：给request实例添加请求拦截器
-requeset.interceptors.request.use((config)=>{
-  
+requeset.interceptors.request.use((config) => {
   // 返回配置对象
-  return config;
+  return config
 })
 
-
 // 第三步：给request实例添加响应拦截器
-requeset.interceptors.response.use((response)=>{
+requeset.interceptors.response.use(
+  (response) => {
     // 成功的回调
     // 简化数据
     return response.data
   },
-  (error)=>{
+  (error) => {
     // 失败的回调：处理http网络错误
     // 定义一个变量，存储网络错误信息
-    let message = '';
+    let message = ''
     // http状态码
     let status = error.response.status
-    switch(status){
+    switch (status) {
       case 401:
-        message = 'TOKEN过期';
-        break;
+        message = 'TOKEN过期'
+        break
       case 403:
-        message = '无权访问';
-        break;
+        message = '无权访问'
+        break
       case 404:
-        message = '请求地址错误';
-        break;
+        message = '请求地址错误'
+        break
       case 500:
-        message = '服务器出现问题';
-        break;
+        message = '服务器出现问题'
+        break
       default:
-        message = '网络出现错误';
-        break;
+        message = '网络出现错误'
+        break
     }
     // 提示错误信息
     ElMessage({
-      type:'error',
-      message
+      type: 'error',
+      message,
     })
-    return Promise.reject(error);
-  }
+    return Promise.reject(error)
+  },
 )
 
 // 对外暴露
-export default requeset;
+export default requeset
