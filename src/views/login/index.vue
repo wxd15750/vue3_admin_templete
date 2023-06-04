fdaf8da2716cc03b43cd75cbeaf6afa1db216e5b
汪~汪~汪~ <w15755583882022@163.com>
2023-06-04 22:31:31 +0800

    feat: 提交代码login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,17 +8,21 @@
           <h1>Hello</h1>
           <h2>欢迎来到登录页</h2>
           <el-form-item>
-            <el-input type="text" :prefix-icon = "User" v-model="loginFrom.username" ></el-input>
+            <el-input type="text" :prefix-icon="User" v-model="loginFrom.username"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-input type="password" :prefix-icon = "Lock" show-password v-model="loginFrom.password"></el-input>
+            <el-input
+              type="password"
+              :prefix-icon="Lock"
+              show-password
+              v-model="loginFrom.password"
+            ></el-input>
           </el-form-item>
           <el-form-item>
             <el-button class="login_btn" type="primary" size="default">登录</el-button>
           </el-form-item>
         </el-form>
         <!-- 登录的表单 -->
-       
       </el-col>
     </el-row>
   </div>
@@ -30,40 +34,39 @@ export default defineComponent({
 })
 </script>
 <script lang="ts" setup>
-import { User,Lock } from '@element-plus/icons-vue'
+import { User, Lock } from '@element-plus/icons-vue'
 // 收集表单数据
-import {reactive, ref} from 'vue'
+import { reactive, ref } from 'vue'
 // 第一种方法：通过ref去定义数据
 // const username = ref('')
 // const password = ref('')
 // 第二种：通过reactive去定义数据
-const loginFrom = reactive({username:'',password:''})
-
+const loginFrom = reactive({ username: '', password: '' })
 </script>
 
-<style scoped lang="scss" >
-.login_container{
+<style scoped lang="scss">
+.login_container {
   width: 100%;
   height: 100vh;
   background: url('@/assets/images/background.jpg') no-repeat;
   background-size: cover;
-  .login_form{
+  .login_form {
     position: relative;
     width: 80%;
-    top:30vh;
+    top: 30vh;
     background: url('@/assets/images/login_form.png') no-repeat;
     background-size: cover;
     padding: 40px;
-    h1{
+    h1 {
       font-size: 40px;
       color: #fff;
     }
-    h2{
+    h2 {
       font-size: 20px;
       color: #fff;
       margin: 20px 0;
     }
-    .login_btn{
+    .login_btn {
       width: 100%;
     }
   }
