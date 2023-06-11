c7de66a53385eb2019859fab0ba292c631519366
汪~汪~汪~ <w15755583882022@163.com>
2023-06-11 23:11:17 +0800

    feat: 设置layout组件，定义首页布局

---
+++ src/layout/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="layout_container">
+    <!-- 左侧菜单 -->
+    <div class="layout_slider">123</div>
+    <!-- 顶部导航 -->
+    <div class="layout_tabbar">456</div>
+    <!-- 内容展示区 -->
+    <div class="layout_main">
+      <p style="height: 3000px;">789</p>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  export default defineComponent({
+    name:''
+  })
+</script>
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+.layout_container{
+  width: 100%;
+  height: 100vh;
+  // background-color: #e84a4a;
+  .layout_slider{
+    width: $base-menu-width;
+    height: 100vh;
+    background-color: $base-menu-background;
+  }
+  .layout_tabbar{
+    position: fixed;
+    top: 0px;
+    left: $base-menu-width;
+    width: calc(100% - $base-menu-width);
+    height: $base-tabber-height;
+    background-color: #45c1ea;
+  }
+  .layout_main{
+    position: absolute;
+    width: calc(100% - $base-menu-width);
+    height: calc(100vh - $base-tabber-height);
+    background-color: #48ca62;
+    left: $base-menu-width;
+    top: $base-tabber-height;
+    padding: 20px;
+    overflow: auto;
+  }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -15,7 +15,7 @@ const router = createRouter({
     {
       // 首页
       path: '/',
-      component: () => import('@/views/home/index.vue'),
+      component: () => import('@/layout/index.vue'),
       name: 'Home',
     },
     {
