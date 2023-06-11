359d38b59afc4733b24e3bf526e5abdb96cc64da
汪~汪~汪~ <w15755583882022@163.com>
2023-06-11 23:33:37 +0800

    feat: logo组件的搭建

--- src/layout/logo/index.vue
+++ src/layout/logo/index.vue
@@ -1,34 +1,34 @@
 <template>
   <div class="logo" v-if="setting.logoHidden">
-    <img  :src="setting.logo" alt="">
-    <p>{{setting.title}}</p>
+    <img :src="setting.logo" alt="" />
+    <p>{{ setting.title }}</p>
   </div>
 </template>
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  export default defineComponent({
-    name:''
-  })
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: '',
+})
 </script>
 <script lang="ts" setup>
 // 引入设置标题于logo的配置文件
 import setting from '@/setting'
-import { set } from 'nprogress';
+import { set } from 'nprogress'
 </script>
 
 <style lang="scss" scoped>
-.logo{
+.logo {
   width: 100%;
   height: $base-menu-logo-height;
-  color:#fff;
+  color: #fff;
   display: flex;
   align-items: center;
   padding: 20px;
-  img{
+  img {
     width: 40px;
     height: 40px;
   }
-  p{
+  p {
     font-size: $basee-logo-title-fontSize;
     margin-left: 10px;
   }
