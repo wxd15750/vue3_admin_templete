adb7bd9aa2c187c44d3a38003db0027253d676ec
汪~汪~汪~ <w15755583882022@163.com>
2023-06-16 07:56:49 +0800

    feat: 首页的搭建及数据的显示

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>
-    <el-card>
-      <h1>我是首页</h1>
-    </el-card>
+  <el-card>
+    <div class="box">
+      <img class="avatar" :src="userStore.avatar" alt="" srcset="" />
+      <div class="bottom">
+        <h3 class="title">{{ getTime() }} 好呀 {{ userStore.username }}</h3>
+        <p class="subtitle">{{ setting.title }}</p>
+      </div>
+    </div>
+  </el-card>
+  <div class="bgcImg">
+    <svg-icon name="welcome" width="600px" height="400px"></svg-icon>
   </div>
 </template>
 <script lang="ts">
@@ -13,10 +20,14 @@ export default defineComponent({
 </script>
 <script lang="ts" setup>
 // import { onMounted } from 'vue'
-// // 引入仓库
-// import useUserStore from '@/store/modules/user'
-// // 获取仓库
-// let userStore = useUserStore()
+// 引入时间相关的文件
+import { getTime } from '@/utils/timer'
+// 引入setting文件
+import setting from '@/setting'
+// 引入仓库
+import useUserStore from '@/store/modules/user'
+// 获取仓库
+let userStore = useUserStore()
 // // 首页挂载完获取用户信息
 // onMounted(() => {
 //   userStore.userInfo()
@@ -24,7 +35,29 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.box-card2 {
-  margin-top: 30px;
+.box {
+  display: flex;
+  .avatar {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+  .bottom {
+    margin-left: 20px;
+    .title {
+      font-size: 30px;
+      font-weight: 900;
+      margin-bottom: 30px;
+    }
+  }
+  .subtitle {
+    font-style: italic;
+    color: #76cf86;
+  }
+}
+.bgcImg {
+  display: flex;
+  margin-top: 10px;
+  justify-content: center;
 }
 </style>
