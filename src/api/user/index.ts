9aad284a5d55087dc74d11f96cedce48e6d7ddee
汪~汪~汪~ <w15755583882022@163.com>
2023-06-17 13:01:33 +0800

    feat: 定义登录接口的类型和API

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -2,23 +2,19 @@ import request from '@/utils/request'
 import { ResponseData, UserInfoType, loginType } from './type'
 
 // 接口地址
-enum userAPI{
+enum userAPI {
   USER_INFO = '/admin/acl/index/info',
   LOGIN_API = '/admin/acl/index/login',
-  LOGOUT_API = '/admin/acl/index/logout'
+  LOGOUT_API = '/admin/acl/index/logout',
 }
-
 
 // 获取用户信息
 
-export const reqUserInfo = () => request.get<any,UserInfoType>(userAPI.USER_INFO)
+export const reqUserInfo = () => request.get<any, UserInfoType>(userAPI.USER_INFO)
 
 // 登录
-export const reqLogin = (data:loginType) => request.post<any,ResponseData>(userAPI.LOGIN_API,data)
+export const reqLogin = (data: loginType) =>
+  request.post<any, ResponseData>(userAPI.LOGIN_API, data)
 
 // 退出登录
-export const reqLogout = () => request.post<any,null>(userAPI.LOGOUT_API)
-
-
-
-
+export const reqLogout = () => request.post<any, null>(userAPI.LOGOUT_API)
--- src/api/user/type.ts
+++ src/api/user/type.ts
@@ -1,29 +1,28 @@
-
 // 定义接口类型
 // 登录的接口类型
-export interface loginType{
+export interface loginType {
   password: string
   username: string
 }
-export interface ResponseData{
+export interface ResponseData {
   code: number
   message: string
-  data:DataType
+  data: DataType
 }
-interface DataType{
-  token:string
+interface DataType {
+  token: string
 }
 
 // 用户信息相关的接口
 export interface UserInfoType {
   code: number
-  message:string
-  data:UserDataType
+  message: string
+  data: UserDataType
 }
-interface UserDataType{
-  routes:string[]
-  buttons:string[]
-  roles:string[]
-  name:string
-  avatar:string
+interface UserDataType {
+  routes: string[]
+  buttons: string[]
+  roles: string[]
+  name: string
+  avatar: string
 }
