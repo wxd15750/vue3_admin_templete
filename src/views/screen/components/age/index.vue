c36160bfb6b6eda2a24b35c622427d330037d5e6
汪~汪~汪~ <w15755583882022@163.com>
2023-06-23 15:06:17 +0800

    feat: 饼图的制作与实现

--- src/views/screen/components/age/index.vue
+++ src/views/screen/components/age/index.vue
@@ -1,12 +1,98 @@
 <template>
-  <div>年龄比例</div>
+  <div class="box2">
+    <div class="title">
+      <p>年龄比例</p>
+      <img src="../../images/dataScreen-title.png" alt="" />
+    </div>
+    <div class="charts" ref="charts"></div>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
-  name: '',
+  name: 'Age',
 })
 </script>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+// 导入echarts图表
+import * as echarts from 'echarts'
 
-<style lang="scss" scoped></style>
+const charts = ref()
+
+onMounted(() => {
+  let mycharts = echarts.init(charts.value)
+
+  let option = {
+    tooltip: {
+      trigger: 'item',
+    },
+    legend: {
+      top: 40,
+      right: 30,
+      orient: 'vertical', // 图例组件方向的位置
+      textStyle: {
+        color: '#fff',
+        fontSize: 14,
+      },
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        itemStyle: {
+          borderRadius: 10,
+          borderColor: '#fff',
+          borderWidth: 2,
+        },
+        label: {
+          show: true,
+          position: 'inside',
+          color: '#fff',
+        },
+
+        labelLine: {
+          show: false,
+        },
+        data: [
+          { value: 1048, name: '军事' },
+          { value: 735, name: '新闻' },
+          { value: 580, name: '娱乐' },
+          { value: 484, name: '直播' },
+          { value: 300, name: '财经' },
+        ],
+      },
+    ],
+    // 调整图表的位置
+    grid: {
+      left: 0,
+      top: 0,
+      right: 0,
+      bottom: 0,
+    },
+  }
+  mycharts.setOption(option)
+})
+</script>
+
+<style lang="scss" scoped>
+.box2 {
+  width: 100%;
+  height: 100%;
+  background: url(../../images/dataScreen-main-cb.png) no-repeat;
+  background-size: 100% 100%;
+  margin: 20px 0;
+  .title {
+    margin-left: 20px;
+    p {
+      color: #fff;
+      font-size: 20px;
+    }
+  }
+  .charts {
+    height: 260px;
+  }
+}
+</style>
